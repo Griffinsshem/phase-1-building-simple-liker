d12c8675608df1cb0ee8cec79144cd1406a964a6
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-06-27 12:12:06 +0300

    add building simple liker

--- main.js
+++ main.js
@@ -5,15 +5,44 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const hearts = document.querySelectorAll(".like-glyph");
+const modal = document.getElementById("modal");
+const modalMessage = document.getElementById("modal-message");
+
+
+hearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        if (heart.textContent === EMPTY_HEART) {
+          heart.textContent = FULL_HEART;
+          heart.classList.add("activated-heart");
+        } else {
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch((error) => {
+        modal.classList.remove("hidden");
+        modalMessage.textContent = error;
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
+
+
+
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
